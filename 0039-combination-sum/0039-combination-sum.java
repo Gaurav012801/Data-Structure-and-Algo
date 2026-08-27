866d9bfc5965ad103e5f614f65924b2b98356110
Gaurav Tiwari <127079519+Gaurav012801@users.noreply.github.com>
2026-08-27 11:39:43 -0700

    Time: 2 ms (95.72%), Space: 45.3 MB (96.5%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,23 +2,26 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans=new ArrayList<>();
         List<Integer> tmp=new ArrayList<>();
-        getCombinationSum(candidates,0,target,tmp,ans);
+        solve(candidates,0,target,tmp,ans);
         return ans;
     }
 
-    private void getCombinationSum(int[] arr,int index,int target,List<Integer> tmp,List<List<Integer>> ans){
+    private void solve(int[] arr,int index,int target,List<Integer> tmp,List<List<Integer>> ans){
         if(target==0){
-           ans.add(new ArrayList<>(tmp));
-           return;
+            ans.add(new ArrayList<>(tmp));
+            return;
         }
         if(index==arr.length){
             return;
         }
+        //Do not take
+        solve(arr,index+1,target,tmp,ans);
+        //Take
         if(arr[index]<=target){
             tmp.add(arr[index]);
-            getCombinationSum(arr,index,target-arr[index],tmp,ans);
+            solve(arr,index,target-arr[index],tmp,ans);
+            //Backtracking
             tmp.remove(tmp.size()-1);
         }
-        getCombinationSum(arr,index+1,target,tmp,ans);
     }
 }
