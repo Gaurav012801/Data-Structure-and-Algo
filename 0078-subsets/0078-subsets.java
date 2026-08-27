270965a277bd7552b2592ce467c38103d9808609
Gaurav Tiwari <127079519+Gaurav012801@users.noreply.github.com>
2026-08-26 23:12:38 -0700

    Time: 1 ms (86.51%), Space: 44.3 MB (20.41%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,37 +1,23 @@
 class Solution {
-
-    public void allsubset(
-        int[] arr,
-        int index,
-        int n,
-        List<Integer> tmp,
-        List<List<Integer>> ans
-    ) {
-
-        if (index == n) {
+    private void getAllSubsets(int[] arr,int index,int n,List<Integer> tmp,List<List<Integer>> ans){
+        //Base condition
+        if(index==n){
             ans.add(new ArrayList<>(tmp));
             return;
         }
 
-        // Don't take arr[index]
-        allsubset(arr, index + 1, n, tmp, ans);
-
-        // Take arr[index]
+        //Don't take it
+        getAllSubsets(arr,index+1,n,tmp,ans);
+        //Take it
         tmp.add(arr[index]);
-
-        allsubset(arr, index + 1, n, tmp, ans);
-
-        // Backtrack
-        tmp.remove(tmp.size() - 1);
+        getAllSubsets(arr,index+1,n,tmp,ans);
+        //BackTracking
+        tmp.remove(tmp.size()-1);
     }
-
     public List<List<Integer>> subsets(int[] nums) {
-
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> tmp = new ArrayList<>();
-
-        allsubset(nums, 0, nums.length, tmp, ans);
-
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> tmp=new ArrayList<>();
+        getAllSubsets(nums,0,nums.length,tmp,ans);
         return ans;
     }
 }
